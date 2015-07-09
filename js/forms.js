e8f4b681aaa4149f773263b2683b117412ee0287
Tomas Kavka <iam@tomaskavka.cz>
2015-07-09 16:21:45 +0200

    Inputs and checkbox lever changed

--- js/forms.js
+++ js/forms.js
@@ -9,105 +9,41 @@
       .on('blur', function() {
         if ($(this).attr('id') === 'firstName') {
           $(this).parent().addClass('valid');
-          $('.help-text', $(this).parent()).removeClass('hint').addClass('success').html('Skvěle, křestní jméno Vašeho dítěte je v pořádku!');
+          $('.help-text', $(this).parent()).removeClass('hint').addClass('success').html("Great, your child's first name is OK!");
+        }
+        else if ($(this).attr('id') === 'favouritePlayer') {
+          $(this).parent().addClass('valid');
+          $('.help-text', $(this).parent()).removeClass('hint').addClass('success').html("Great, your child's favourite player is OK!");
         }
         else {         
           $(this).parent().addClass('invalid');
-          $('.help-text', $(this).parent()).removeClass('hint').addClass('error').html('Vyplňte prosím příjmení Vašeho dítěte, např.: <strong>Šebrle</strong>');
+          if ($(this).parent().hasClass('optional')) {
+            $('.help-text', $(this).parent()).removeClass('hint').addClass('warning').html("Your child's favourite football team is optional. Valid is e.g. <strong><i>Tavares</i></strong>.");
+          }
+          else {
+            $('.help-text', $(this).parent()).removeClass('hint').addClass('error').html("Surname is mandatory. Valid is e.g. <strong><i>Slavia Prague</i></strong>.");
+          }
         }
         $(this).parent().removeClass('focus');
         $(this).parent().removeClass('pristine');
         $(this).parent().addClass('dirty');
       });
-    // Function to update labels of text fields
-    /*Materialize.updateTextFields = function() {
-      var input_selector = 'input[type=text], input[type=password], input[type=email], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
-      $(input_selector).each(function(index, element) {
-        if ($(element).val().length > 0 || $(this).attr('placeholder') !== undefined || $(element)[0].validity.badInput === true) {
-          $(this).siblings('label, i').addClass('active');
+
+    $('.switch label').on('click', function(e) {
+      var target = $(e.target);
+
+      if (!target.hasClass('lever') && !target.hasClass('checkbox')) {
+        e.preventDefault();
+        var checkbox = $('.checkbox', this);
+
+        if (target.hasClass('on')) {
+          checkbox.prop('checked', true);
         }
-        else {
-          $(this).siblings('label, i').removeClass('active');
-        }
-      });
-    };
-
-    // Text based inputs
-    var input_selector = 'input[type=text], input[type=password], input[type=email], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
-
-    // Handle HTML5 autofocus
-    $('input[autofocus]').siblings('label, i').addClass('active');
-
-    // Add active if form auto complete
-    $(document).on('change', input_selector, function () {
-      if($(this).val().length !== 0 || $(this).attr('placeholder') !== undefined) {
-        $(this).siblings('label, i').addClass('active');
-      }
-      validate_field($(this));
-    });
-
-    // Add active if input element has been pre-populated on document ready
-    $(document).ready(function() {
-      Materialize.updateTextFields();
-    });
-
-    // HTML DOM FORM RESET handling
-    $(document).on('reset', function(e) {
-      var formReset = $(e.target);
-      if (formReset.is('form')) {
-        formReset.find(input_selector).removeClass('valid').removeClass('invalid');
-        formReset.find(input_selector).each(function () {
-          if ($(this).attr('value') === '') {
-            $(this).siblings('label, i').removeClass('active');
-          }
-        });
-
-        // Reset select
-        formReset.find('select.initialized').each(function () {
-          var reset_text = formReset.find('option[selected]').text();
-          formReset.siblings('input.select-dropdown').val(reset_text);
-        });
-      }
-    });
-
-    // Add active when element has focus
-    $(document).on('focus', input_selector, function () {
-      $(this).siblings('label, i').addClass('active');
-    });
-
-    $(document).on('blur', input_selector, function () {
-      var $inputElement = $(this);
-      if ($inputElement.val().length === 0 && $inputElement[0].validity.badInput !== true && $inputElement.attr('placeholder') === undefined) {
-        $inputElement.siblings('label, i').removeClass('active');
-      }
-      validate_field($inputElement);
-    });
-
-    validate_field = function(object) {
-      var hasLength = object.attr('length') !== undefined;
-      var lenAttr = parseInt(object.attr('length'));
-      var len = object.val().length;
-
-      if (object.val().length === 0 && object[0].validity.badInput === false) {
-        if (object.hasClass('validate')) {
-          object.removeClass('valid');
-          object.removeClass('invalid');
+        else if (target.hasClass('off')) {
+          checkbox.prop('checked', false);
         }
       }
-      else {
-        if (object.hasClass('validate')) {
-          // Check for character counter attributes
-          if ((object.is(':valid') && hasLength && (len < lenAttr)) || (object.is(':valid') && !hasLength)) {
-            object.removeClass('invalid');
-            object.addClass('valid');
-          }
-          else {
-            object.removeClass('valid');
-            object.addClass('invalid');
-          }
-        }
-      }
-    };*/
+    });
 
 
     // Textarea Auto Resize
